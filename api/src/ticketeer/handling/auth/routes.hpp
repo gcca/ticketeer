cb70dbb306b6c5bb5e6b26fb59a4f79602ec530b
gcca <git@gcca.dev>
2026-04-26 20:17:00 -0500

    20260426201700

--- api/src/ticketeer/handling/auth/routes.hpp
+++ api/src/ticketeer/handling/auth/routes.hpp
@@ -2,6 +2,8 @@
 
 #include <drogon/HttpController.h>
 
+#include "ticketeer/handling/auth/middlewares.hpp"
+
 namespace ticketeer::api {
 
 class Auth : public drogon::HttpController<Auth> {
@@ -9,8 +11,10 @@ public:
   METHOD_LIST_BEGIN
   METHOD_ADD(Auth::SignIn, "/v1/signin", drogon::Post);
   METHOD_ADD(Auth::SignIn_Overlord, "/v1/signin/overlord", drogon::Post);
-  METHOD_ADD(Auth::Me, "/v1/me", drogon::Get);
-  METHOD_ADD(Auth::SignOut, "/v1/signout", drogon::Post);
+  METHOD_ADD(Auth::Me, "/v1/me", drogon::Get,
+             "ticketeer::api::auth::middlewares::LogInRequired");
+  METHOD_ADD(Auth::SignOut, "/v1/signout", drogon::Post,
+             "ticketeer::api::auth::middlewares::LogInRequired");
   METHOD_LIST_END
 
   void SignIn(const drogon::HttpRequestPtr &req,
--- api/src/ticketeer/handling/role/requester/routes.hpp
+++ api/src/ticketeer/handling/role/requester/routes.hpp
@@ -2,19 +2,36 @@
 
 #include <drogon/HttpController.h>
 
+#include "ticketeer/handling/auth/middlewares.hpp"
+#include "ticketeer/handling/role/requester/middlewares.hpp"
+
 namespace ticketeer::api::role {
 
 class Requester : public drogon::HttpController<Requester> {
 public:
   METHOD_LIST_BEGIN
-  METHOD_ADD(Requester::TicketList, "/v1/ticket/list", drogon::Get);
-  METHOD_ADD(Requester::TicketCreate, "/v1/ticket/create", drogon::Post);
+  METHOD_ADD(Requester::TicketList, "/v1/ticket/list", drogon::Get,
+             "ticketeer::api::auth::middlewares::LogInRequired",
+             "ticketeer::api::role::requester::middlewares::"
+             "RoleRequesterRequired");
+  METHOD_ADD(Requester::TicketCreate, "/v1/ticket/create", drogon::Post,
+             "ticketeer::api::auth::middlewares::LogInRequired",
+             "ticketeer::api::role::requester::middlewares::"
+             "RoleRequesterRequired");
   METHOD_ADD(Requester::TicketDetails, "/v1/ticket/{ticket_id}/details",
-             drogon::Get);
+             drogon::Get, "ticketeer::api::auth::middlewares::LogInRequired",
+             "ticketeer::api::role::requester::middlewares::"
+             "RoleRequesterRequired");
   METHOD_ADD(Requester::TicketActivityList,
-             "/v1/ticket/{ticket_id}/activity/list", drogon::Get);
+             "/v1/ticket/{ticket_id}/activity/list", drogon::Get,
+             "ticketeer::api::auth::middlewares::LogInRequired",
+             "ticketeer::api::role::requester::middlewares::"
+             "RoleRequesterRequired");
   METHOD_ADD(Requester::TicketActivityCreateMessage,
-             "/v1/ticket/{ticket_id}/activity/create/message", drogon::Post);
+             "/v1/ticket/{ticket_id}/activity/create/message", drogon::Post,
+             "ticketeer::api::auth::middlewares::LogInRequired",
+             "ticketeer::api::role::requester::middlewares::"
+             "RoleRequesterRequired");
   METHOD_LIST_END
 
   void
